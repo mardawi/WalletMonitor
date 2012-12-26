4e0974d58600bf2923c9ac51eb76de90a9db9d33
Batool <batool@batool-desktop.(none)>
2012-12-26 15:29:56 +0200

    by tag by category

--- Resources/ui/common/reports/CategoryReport.js
+++ Resources/ui/common/reports/CategoryReport.js
@@ -6,8 +6,14 @@ function CategoryReport(_title, _container) {
 		title : 'Category Report'
 	});
 
+	var isAndroid = (Ti.Platform.osname == 'android');
+	var dataExp = [];
+
 	var picker = Ti.UI.createPicker({
-		top : 50
+		top : 50,
+		height : isAndroid ? '15%' : '50%',
+		top : '2%',
+		width:'90%'
 	});
 
 	var db = require('lib/db');
@@ -23,9 +29,61 @@ function CategoryReport(_title, _container) {
 	}
 
 	picker.addEventListener('change', function(e) {
-		Ti.UI.createAlertDialog({
-			message : e.row.category.title + ' selected'
-		}).show();
+
+		dataExp = db.getTransByCategory(e.row.category.title);
+		var tableCustomRows = [];
+
+		for (var i = 0; i < dataExp.length; i++) {
+			var row = Ti.UI.createTableViewRow({
+				height : '70',
+				data : dataExp[i]
+			});
+
+			if (i % 2 == 0)
+				row.backgroundColor = '#999';
+			else
+				row.backgroundColor = '#777';
+
+			var leftPart = Ti.UI.createView({
+				layout : 'vertical',
+				left : '0',
+				width : '50%'
+			});
+			leftPart.add(Ti.UI.createLabel({
+				text : 'Expense',
+				left : '5%',
+				color : '#d71527'
+			}));
+			leftPart.add(Ti.UI.createLabel({
+				text : dataExp[i].amount + ' JD',
+				left : '5%'
+			}));
+			row.add(leftPart);
+
+			var rightPart = Ti.UI.createView({
+				layout : 'vertical',
+				right : '0',
+				width : '50%'
+			});
+
+			var obj = db.getCategory(dataExp[i].categoryId);
+
+			rightPart.add(Ti.UI.createLabel({
+				text : obj.title,
+				right : '5%',
+				color : 'black'
+			}));
+
+			rightPart.add(Ti.UI.createLabel({
+				text : dataExp[i].date,
+				right : '5%'
+			}))
+
+			row.add(rightPart);
+			tableCustomRows.push(row);
+		};
+
+		transTable.data = tableCustomRows;
 	});
 
 	picker.add(data);
@@ -33,75 +91,11 @@ function CategoryReport(_title, _container) {
 
 	win.add(picker);
 
-	var scrollView = Ti.UI.createScrollView({
-		scrollType : 'vertical',
-		width : '90%',
-		top : '2%'
-	});
-
-	win.add(scrollView);
-
-	var dataExp = db.recentExpenses();
-
 	var transTable = Ti.UI.createTableView({
 		title : 'Recent',
 		height : '100%',
 	});
-
-	var tableCustomRows = [];
-
-	for (var i = 0; i < dataExp.length; i++) {
-		var row = Ti.UI.createTableViewRow({
-			height : '70',
-			data : dataExp[i]
-		});
-
-		if (i % 2 == 0)
-			row.backgroundColor = '#999';
-		else
-			row.backgroundColor = '#777';
-
-		var leftPart = Ti.UI.createView({
-			layout : 'vertical',
-			left : '0',
-			width : '50%'
-		});
-		leftPart.add(Ti.UI.createLabel({
-			text : 'Expense',
-			left : '5%',
-			color : '#d71527'
-		}));
-		leftPart.add(Ti.UI.createLabel({
-			text : dataExp[i].amount + ' JD',
-			left : '5%'
-		}));
-		row.add(leftPart);
-
-		var rightPart = Ti.UI.createView({
-			layout : 'vertical',
-			right : '0',
-			width : '50%'
-		});
-
-		var obj = db.getCategory(dataExp[i].categoryId);
-
-		rightPart.add(Ti.UI.createLabel({
-			text : obj.title,
-			right : '5%',
-			color : 'black'
-		}));
-
-		rightPart.add(Ti.UI.createLabel({
-			text : dataExp[i].date,
-			right : '5%'
-		}))
-
-		row.add(rightPart);
-		tableCustomRows.push(row);
-	};
-
-	transTable.data = tableCustomRows;
-	scrollView.add(transTable);
+	win.add(transTable);
 
 	transTable.addEventListener('click', function(e) {
 
--- Resources/ui/common/reports/TagReport.js
+++ Resources/ui/common/reports/TagReport.js
@@ -1,8 +1,112 @@
 function TagReport(_title, _container) {
 
-var win = Ti.UI.createWindow({
-	
-});
-return win;
+	var win = Ti.UI.createWindow({
+		layout : 'vertical',
+		backgroundColor : '#ccc',
+		title : 'Tag Report'
+	});
+
+	var isAndroid = (Ti.Platform.osname == 'android');
+	var dataExp = [];
+
+	var picker = Ti.UI.createPicker({
+		top : 50,
+		height : isAndroid ? '15%' : '50%',
+		top : '2%',
+		width:'90%'
+	});
+
+	var db = require('lib/db');
+	var Tags = db.getTags();
+
+	var data = [];
+
+	for (var i = 0; i < Tags.length; i++) {
+		data.push(Ti.UI.createPickerRow({
+			title : Tags[i].title,
+			tagName : Tags[i].title
+		}));
+	}
+
+	picker.addEventListener('change', function(e) {
+
+		dataExp = db.getTransByTag(e.row.tagName);
+		var tableCustomRows = [];
+
+		for (var i = 0; i < dataExp.length; i++) {
+			var row = Ti.UI.createTableViewRow({
+				height : '70',
+				data : dataExp[i]
+			});
+
+			if (i % 2 == 0)
+				row.backgroundColor = '#999';
+			else
+				row.backgroundColor = '#777';
+
+			var leftPart = Ti.UI.createView({
+				layout : 'vertical',
+				left : '0',
+				width : '50%'
+			});
+			leftPart.add(Ti.UI.createLabel({
+				text : 'Expense',
+				left : '5%',
+				color : '#d71527'
+			}));
+			leftPart.add(Ti.UI.createLabel({
+				text : dataExp[i].amount + ' JD',
+				left : '5%'
+			}));
+			row.add(leftPart);
+
+			var rightPart = Ti.UI.createView({
+				layout : 'vertical',
+				right : '0',
+				width : '50%'
+			});
+
+			var obj = db.getCategory(dataExp[i].categoryId);
+
+			rightPart.add(Ti.UI.createLabel({
+				text : obj.title,
+				right : '5%',
+				color : 'black'
+			}));
+
+			rightPart.add(Ti.UI.createLabel({
+				text : dataExp[i].date,
+				right : '5%'
+			}))
+
+			row.add(rightPart);
+			tableCustomRows.push(row);
+		};
+
+		transTable.data = tableCustomRows;
+	});
+
+	picker.add(data);
+	picker.selectionIndicator = true;
+
+	win.add(picker);
+
+	var transTable = Ti.UI.createTableView({
+		title : 'Recent',
+		height : '100%',
+	});
+	win.add(transTable);
+
+	transTable.addEventListener('click', function(e) {
+
+		var obj = e.rowData.data;
+
+		var ExpenseDetailWindow = require('ui/common/transactions/ExpenseDetails');
+		var detailedExpense = new ExpenseDetailWindow('Expense', _container, obj);
+		_container.open(detailedExpense);
+
+	});
+
+	return win;
 };
 module.exports = TagReport;
