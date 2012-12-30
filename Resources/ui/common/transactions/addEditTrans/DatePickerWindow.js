52255e2ffcc68ef2d8b558d464433ed22c3d065e
Batool <batool@batool-desktop.(none)>
2012-12-30 15:27:07 +0200

    time picker for android

--- Resources/ui/common/transactions/addEditTrans/DatePickerWindow.js
+++ Resources/ui/common/transactions/addEditTrans/DatePickerWindow.js
@@ -1,27 +1,45 @@
 function DatePickerWindow(_title, _container) {
-	
+
 	var win = Ti.UI.createWindow({
-		title:'Set Date',
-		backgroundColor:'#ccc'
+		title : 'Set Date',
+		backgroundColor : '#ccc',
+		layout : 'horizontal'
 	});
+
+	var isAndroid = Ti.Platform.osname == 'android';
 
 	var value = new Date();
 	var minDate = new Date();
-	minDate.setFullYear(value.getFullYear()-1);
+	minDate.setFullYear(value.getFullYear() - 1);
 	minDate.setMonth(1);
 	minDate.setDate(1);
 
-	
-
 	var picker = Ti.UI.createPicker({
-		type: Ti.UI.PICKER_TYPE_DATE_AND_TIME,
+		type : isAndroid ? Ti.UI.PICKER_TYPE_DATE : Ti.UI.PICKER_TYPE_DATE_AND_TIME,
 		minDate : minDate,
 		maxDate : value,
-		value : value
+		value : value,
+		top : '10%'
 	});
 	picker.selectionIndicator = true;
 
 	win.add(picker);
+
+	if (isAndroid) {
+		var androidTimePicker = Ti.UI.createPicker({
+			type : Ti.UI.PICKER_TYPE_TIME,
+			width : '50%',
+			top : '10%'
+		});
+
+		picker.width = '50%';
+		win.add(androidTimePicker);
+
+		androidTimePicker.addEventListener('change', function(e) {
+			_container.timeValue = e.value;
+			Ti.App.fireEvent('TimeChanged');
+		});
+	}
 
 	picker.addEventListener('change', function(e) {
 		_container.dateValue = e.value;
